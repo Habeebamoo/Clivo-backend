320ee7fdb0c458ceb26f28a9b7390172ca47a9f3
Habeebamoo <habeebamoo08@gmail.com>
2025-09-26 14:09:24 +0100

    designed users & profiles table

--- internal/database/migrations/20250922172906_add_users_table.sql
+++ internal/database/migrations/20250922172906_add_users_table.sql
@@ -1,17 +1,31 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE users (
-  user_id PRIMARY KEY,
+  user_id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT NOT NULL,
+  role TEXT NOT NULL,
+  verified BOOLEAN DEFAULT f,
   created_at TIMESTAMP DEFAULT NOW(),
 
   UNIQUE(user_id),
   UNIQUE(email)
 );
+
+CREATE TABLE profiles (
+  user_id TEXT PRIMARY KEY,
+  username TEXT NOT NULL,
+  bio TEXT,
+  picture TEXT,
+  following INTEGER,
+  followers INTEGER,
+
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP TABEL profiles;
 DROP TABLE users;
 -- +goose StatementEnd
--- internal/models/user.go
+++ internal/models/user.go
@@ -6,13 +6,15 @@ type User struct {
 	UserId     string     `json:"userId"`
 	Name       string     `json:"name"`
 	Email      string     `json:"email"`
-	Username   string     `json:"username"`
 	Role       string     `json:"role"`
+	Verified   bool       `json:"verified"`
 	CreatedAt  time.Time  `json:"createdAt"`
 }
 
 type Profile struct {
 	UserId     string  `json:"userId"`
+	Username   string  `json:"username"`
+	Bio        string  `json:"bio"`
 	Picture    string  `json:"picture"`
 	Following  int     `json:"following"`
 	Followers  int     `json:"followers"`
